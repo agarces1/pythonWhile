09ae0002f56d754fdb81489d675edd010833578e
adria <adria@ADRLT3>
2016-12-11 08:10:30 -0600

    While vs Loop

---
+++ whileFunction/whileFunction/whileFunction.py
@@ -0,0 +1,14 @@
+#while loops can be used instead of for loops
+import turtle
+counter = 0
+while counter < 4:
+    turtle.forward(100)
+    turtle.right(90)
+    counter = counter+1 #or += 1 will work the same way
+#answer = '0'
+#while answer !='42':
+#    answer = input('What is the meaning of life?');
+#print('Congrats you are right!!');
+#While loop make sure your condition changes
+#use for loops when ever possible
+#dont fear the while loop, They are used to read data until no data.
